db4125150fdaf5016c90b51fe8384eaa47be06c3
ludovic <ludovic.angenard@orange.fr>
2022-06-30 10:29:42 +0200

    add route details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { ModuleDetailsComponent } from './module-details/module-details.component';
 
 const routes: Routes = [
-  { path: 'connexion', component: ConnexionComponent }
-
-];
-
+  { path: '', redirectTo: 'connexion', pathMatch: 'full' },
+  { path: 'connexion', component: ConnexionComponent },
+  { path: 'details', component: ModuleDetailsComponent },
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
