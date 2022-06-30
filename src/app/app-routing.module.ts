94a5013183e7b27d3e8d15de3e4fac5a7a369afc
ludovic <ludovic.angenard@orange.fr>
2022-06-30 09:50:45 +0200

    add details route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { SignatureComponent } from './signature/signature.component';
 import { PlanningComponent } from './planning/planning.component';
+import { ModuleDetailsComponent } from './module-details/module-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'connexion', pathMatch: 'full' },
   { path: 'connexion', component: ConnexionComponent },
   { path: 'signature', component: SignatureComponent },
   { path: 'planning', component: PlanningComponent },
+  { path: 'details', component: ModuleDetailsComponent },
 ]
 
 @NgModule({
